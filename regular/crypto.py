b6f5360d5dc38bd815dd5272d20a53400d590feb
Sukjun Kim <dandyrilla@naver.com>
2023-01-11 02:56:44 +0900

    Create an instance of Upbit
    - Keep a connection to the upbit API in the global scope
    - Add utility functions in util/fileio.py
    - Add an example configuration file containing keys and weights

--- regular/crypto.py
+++ regular/crypto.py
@@ -1,7 +1,16 @@
+import pyupbit
 from flask import Blueprint
+
+from regular.util.fileio import json_load
 
 
 bp = Blueprint('crypto', __name__, url_prefix='/crypto')
+
+
+config = json_load('regular/config.json')
+access_key = config['access_key']
+secret_key = config['secret_key']
+upbit = pyupbit.Upbit(access_key, secret_key)
 
 
 @bp.route('/')
