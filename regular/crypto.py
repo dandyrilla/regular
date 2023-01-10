6af7698b658041aba9a85f3e9c8b8794156cd4bf
Sukjun Kim <dandyrilla@naver.com>
2023-01-11 03:56:10 +0900

    Add a route for showing prices

--- regular/crypto.py
+++ regular/crypto.py
@@ -1,6 +1,6 @@
 import pyupbit
 import pandas as pd
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, request
 
 from regular.util.fileio import json_load
 
@@ -34,9 +34,55 @@ def get_balances(tickers=None):
     return df
 
 
+def get_dummy_prices(fiat):
+    return pd.DataFrame([{
+        'ticker': fiat,
+        'market': f'{fiat}-{fiat}',
+        'price': 1.0,
+    }])
+
+
+def get_current_prices(markets):
+    rows = []
+    data = pyupbit.get_current_price(markets)
+    if len(markets) == 1:
+        market = markets[0]
+        data = {market: data}
+    for market, price in data.items():
+        _, ticker = market.split('-')
+        rows.append({
+            'ticker': ticker,
+            'market': market,
+            'price': price,
+        })
+    df = pd.DataFrame(rows)
+    check_if_all_loaded(df['market'].tolist(), markets)
+    return df
+
+
 @bp.route('/balances')
 def show_balances():
     df = get_balances()
+    return df.to_html()
+
+
+@bp.route('/prices')
+def show_prices():
+    fiat = request.args.get('fiat', 'KRW')
+    ticker = request.args.get('ticker')
+    if ticker is None:
+        default_ticker = 'BTC,ETH,XRP'
+        return redirect(url_for(
+            'crypto.show_prices',
+            fiat=fiat,
+            ticker=default_ticker,
+        ))
+    tickers = ticker.split(',')
+    markets = [f'{fiat}-{ticker}' for ticker in tickers]
+    try:
+        df = get_current_prices(markets)
+    except pyupbit.errors.UpbitError as e:
+        return e.__str__()
     return df.to_html()
 
 
